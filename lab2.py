01a23dc1966a1aefcc6126dda30d8e60fa62fb43
Harrisonguy <hguy@flemingcollege.ca>
2023-02-04 18:31:36 -0500

    Update lab2.py
    Removed the hockey example from the bottom and added line spacing to match the example in lab manual

--- lab2.py
+++ lab2.py
@@ -9,17 +9,17 @@ def main():
         ],
         'movies' : [
             {
-                'title' : 'Oldboy(2003)',
+                'title' : 'oldboy(2003)',
                 'genre' : ['action', 'drama', 'mystery']
             },
             {
-                'title' : 'Parasite',
+                'title' : 'parasite',
                 'genre' : ['drama', 'thriller']
             }
         ] 
     }
-    
-    about_me['movies'].append({'title' : 'Moon', 'genre' : ['drama','mystery', 'sci-fi']}) 
+
+    about_me['movies'].append({'title' : 'moon', 'genre' : ['drama','mystery', 'sci-fi']}) 
     print_student_name_and_id(about_me)
     print_pizza_toppings(about_me)
     add_pizza_toppings(about_me, ('broccoli','SPINACH'))
@@ -34,7 +34,7 @@ def print_student_name_and_id(about_me):
     #last_name not used but felt useful
     last_name = split_student_name[-1]
     student_id = about_me['student_id']
-    print(f'My name is {student_name} but you can call me Sir {first_name}.\nMy student ID is {student_id}.')
+    print(f'My name is {student_name} but you can call me Sir {first_name}.\nMy student ID is {student_id}.\n')
     return
 
 def add_pizza_toppings(about_me, toppings):
@@ -48,6 +48,7 @@ def print_pizza_toppings(about_me):
     print('My favourite pizza toppings are:')
     for p in about_me['pizza_toppings']:
         print(f'- {p}')
+    print()
     return
 
 def print_movie_genres(about_me):
@@ -61,7 +62,8 @@ def print_movie_genres(about_me):
     #adds "and " before printing the last genre
     if len(genres) > 1:
         genres[-1] = "and " + genres[-1]    
-    print('I like to watch', *genres, 'movies.', sep=", ")
+    print('I like to watch', *genres, sep=", ", end=" movies.\n")
+    print()
     return 
 
 def print_movie_titles(movie_list):
@@ -71,94 +73,8 @@ def print_movie_titles(movie_list):
     movie_title = [t['title'].title() for t in movie_list['movies']]
     if len(movie_title) > 1:
         movie_title[-1] = "and " + movie_title[-1]
-    print("Some of my favourite movies are", ", ".join(movie_title), end=".\n")
+    print("Some of my favourite movies are", ", ".join(movie_title), end="!\n")
     return
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-    hockey_team = {
-        'name' : 'Maple Leafs',
-        'city' : 'Toronto',
-        'players' : [
-            'matthews',
-            'marner',
-            'nylander'
-        ],
-        'games' : [
-            {
-                'oppenent' : 'Canadiens',
-                'goals for' : 6,
-                'goals against' :2,
-            },
-             {
-                'oppenent' : 'Ottawa',
-                'goals for' : 2,
-                'goals against' :3,
-            }
-        ]
-    }
-    
-    hockey_team['games'].append({'opponent' : 'Boston', 'goals for' : 8, 'goals against' : 0,})
-    print_team_info(hockey_team)
-    add_players(hockey_team, ('gi','re','en'))
-    print_team_info(hockey_team)
-    print_opponents(hockey_team['games'])
-    
-    return
-
-def print_opponents(team):
-    opponents = []
-    for g in team['games']:
-        opponents.append(g['oppoent'])
-    opponents = [g['opponent'].title() for g in team['games']]
-    print(', '.join(opponents), end='.\n')
-
-    return
-
-def add_players(team, new_players):
-    team['players'].extend(new_players)
-    team['players'].sort()
-    for i,p in enumerate(team['players']):
-        team['players'][i] = p.capitalize()
-
-def print_team_info(team):
-    team_name = team['name'].title()
-    team_city = team['city'].capitalize()
-    print(f'{team_name} {team_city}')
-    for p in team['players']:
-        print(f'- {p.capitalize()}')
-    
-    
-    
-    
-    
-    
-    
-
-
-
-    return
-
-
-if __name__ == '__main__':
-    main()
-    """
