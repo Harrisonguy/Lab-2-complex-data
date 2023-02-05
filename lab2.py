221ec9c08f24343203274657647424c1076c8131
Harrisonguy <hguy@flemingcollege.ca>
2023-02-04 23:08:22 -0500

    Update lab2.py
    fixed , issue after watch in print_movie_genre

--- lab2.py
+++ lab2.py
@@ -31,10 +31,9 @@ def print_student_name_and_id(about_me):
     student_name = about_me['full_name']
     split_student_name = student_name.split(" ")
     first_name = split_student_name[0]
-    #last_name not used but felt useful
-    last_name = split_student_name[-1]
+    last_name = split_student_name[-1] #last_name not used but felt useful
     student_id = about_me['student_id']
-    print(f'My name is {student_name} but you can call me Sir {first_name}.\nMy student ID is {student_id}.\n')
+    print(f'My name is {student_name}, but you can call me Sir {first_name}.\nMy student ID is {student_id}.\n')
     return
 
 def add_pizza_toppings(about_me, toppings):
@@ -52,17 +51,16 @@ def print_pizza_toppings(about_me):
     return
 
 def print_movie_genres(about_me):
-    #I had to use set() because I listed all the genre for the movies and did not want it to list reapting genre
-    genres = set()
+    genres = []
     for movie in about_me['movies']:
         for genre in movie['genre']:
-            genres.add(genre)
-    # made it into a list so I can add then "and " at the end 
-    genres = list(genres)
+            genres.append(genre)
+    #removes duplicates
+    genres = list(set(genres))
     #adds "and " before printing the last genre
     if len(genres) > 1:
         genres[-1] = "and " + genres[-1]    
-    print('I like to watch', *genres, sep=", ", end=" movies.\n")
+    print('I like to watch', ", ".join(genres), end=" movies.\n")
     print()
     return 
 
@@ -71,6 +69,7 @@ def print_movie_titles(movie_list):
     for t in movie_list['movies']:
         movie_title.append(t['title'])
     movie_title = [t['title'].title() for t in movie_list['movies']]
+    #adds "and " before the last movie title
     if len(movie_title) > 1:
         movie_title[-1] = "and " + movie_title[-1]
     print("Some of my favourite movies are", ", ".join(movie_title), end="!\n")
